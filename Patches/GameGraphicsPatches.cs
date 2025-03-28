39fb09a108fb1ae188f07059bea0d75e8b806b76
Sergey Syrtsev <syrtsevser@gmail.com>
2025-03-28 20:18:44 +0300

    Add project files.

---
+++ Patches/GameGraphicsPatches.cs
@@ -0,0 +1,36 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Mugnum.ExpandedFpsLimit.Patches
+{
+	/// <summary>
+	/// Patch for GameGraphicsClass.SetFramerateLimits method.
+	/// </summary>
+	internal class SetFramerateLimitsPatch : ModulePatch
+	{
+		/// <summary>
+		/// Returns target method to override.
+		/// </summary>
+		/// <returns> Target method. </returns>
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(GameGraphicsClass).GetMethod(nameof(GameGraphicsClass.SetFramerateLimits));
+		}
+
+		/// <summary>
+		/// Applies to method after it's execution.
+		/// </summary>
+		[PatchPostfix]
+		private static void PatchPostfix()
+		{
+			var fpsLimit = Plugin.MaxFpsLimit.Value;
+			if (fpsLimit <= 0)
+			{
+				fpsLimit = Plugin.DefaultMaxFramerate;
+			}
+
+			GameGraphicsClass.MaxFramerateGameLimit = fpsLimit;
+			GameGraphicsClass.MaxFramerateLobbyLimit = fpsLimit;
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,74 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using JetBrains.Annotations;
+using Mugnum.ExpandedFpsLimit.Patches;
+using System;
+
+namespace Mugnum.ExpandedFpsLimit
+{
+	/// <summary>
+	/// Expanded FPS limit mod plugin.
+	/// </summary>
+	[BepInPlugin("mugnum", "ExpandedFpsLimit", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+		#region Constants
+
+		/// <summary>
+		/// Default max framerate limit.
+		/// </summary>
+		internal const int DefaultMaxFramerate = 360;
+
+		#endregion Constants
+
+		#region Fields: Internal
+
+		/// <summary>
+		/// Logger.
+		/// </summary>
+		internal static ManualLogSource Log;
+		
+		/// <summary>
+		/// Max framerate limit.
+		/// </summary>
+		internal static ConfigEntry<int> MaxFpsLimit;
+
+		#endregion Fields: Internal
+
+		#region Methods
+
+		/// <summary>
+		/// Initializes the plugin.
+		/// </summary>
+		[UsedImplicitly]
+		internal void Awake()
+		{
+			Log = Logger;
+			Logger.LogInfo("Expanded FPS limit: initializing.");
+
+			try
+			{
+				InitializeConfig();
+				new SetFramerateLimitsPatch().Enable();
+				Logger.LogInfo("Expanded FPS limit: initialized.");
+			}
+			catch (Exception ex)
+			{
+				Logger.LogError($"ExpandedFpsLimit {nameof(Awake)} method. Error: {ex}");
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Initializes configuration.
+		/// </summary>
+		private void InitializeConfig()
+		{
+			const string GeneralSectionName = "General";
+			MaxFpsLimit = Config.Bind(GeneralSectionName, "Max framerate limit", DefaultMaxFramerate, "Sets FPS limit in Graphics settings.");
+		}
+
+		#endregion Methods
+	}
+}
